a42da12424911f1bc37bad04a49ecb2a8445c6a8
Christian Stewart <christian@paral.in>
2016-12-14 19:13:57 -0500

    fix: vversion vs version

--- push.bash
+++ push.bash
@@ -1,5 +1,4 @@
 set -e
 VERSION=latest
-docker build --tag="paralin/dota-replay:v${VERSION}" .
-docker tag paralin/dota-replay:v${VERSION} paralin/dota-replay:v${VERSION}
-gcloud docker -- push paralin/dota-replay:v${VERSION}
+docker build --tag="paralin/dota-replay:${VERSION}" .
+docker tag paralin/dota-replay:${VERSION} paralin/dota-replay:${VERSION}
